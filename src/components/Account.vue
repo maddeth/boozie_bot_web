811c386568006ba1dc0413a46455419a75cbb244
maddeth <20545155+maddeth@users.noreply.github.com>
2024-10-23 22:05:30 +0100

    Add basic files

---
+++ src/components/Account.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { supabase } from '../supabase.js'
+import { ref } from 'vue'
+
+const props = defineProps(['session'])
+
+const loading = ref(true)
+
+
+async function signOut() {
+  try {
+    loading.value = true
+    const { error } = await supabase.auth.signOut()
+    if (error) throw error
+  } catch (error) {
+    alert(error.message)
+  } finally {
+    loading.value = false
+  }
+}
+</script>
+
+<template>
+     <div>
+      <p>Logged in</p>
+     </div>
+    <div>
+      <button class="button block" @click="signOut">Sign Out</button>
+    </div>
+</template>
---
+++ src/components/Auth.vue
@@ -0,0 +1,37 @@
+<script setup>
+import { ref } from 'vue'
+import { supabase } from '../supabase.js'
+
+const loading = ref(false)
+
+const handleLogin = async () => {
+  try {
+    await supabase.auth.signInWithOAuth({
+      provider: 'twitch',
+    })
+  } catch (error) {
+    if (error instanceof Error) {
+      alert(error.message)
+    }
+  } finally {
+    loading.value = false
+  }
+}
+</script>
+
+<template>
+  <form class="row flex-center flex" @submit.prevent="handleLogin">
+    <div class="col-6 form-widget">
+      <h1 class="header">Boozie Bot</h1>
+      <p class="description">Sign in via Twitch</p>
+      <div>
+        <input
+          type="submit"
+          class="button block"
+          :value="loading ? 'Loading' : 'Sign In'"
+          :disabled="loading"
+        />
+      </div>
+    </div>
+  </form>
+</template>
