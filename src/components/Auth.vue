6b4ca9cb852283b6040457724839bead2b53706c
maddeth <20545155+maddeth@users.noreply.github.com>
2024-10-29 19:39:34 +0000

    update login form

--- src/components/Auth.vue
+++ src/components/Auth.vue
@@ -23,8 +23,8 @@ const handleLogin = async () => {
 </script>
 
 <template>
-  <form id="login" class="row flex-center flex" @submit.prevent="handleLogin">
-    <div class="col-6 form-widget">
+  <form class="row flex-center flex" @submit.prevent="handleLogin">
+    <div class="login-form">
       <h1 class="header">Boozie Bot</h1>
       <p class="description">Sign in via Twitch</p>
       <div>
