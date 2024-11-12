8e4e7da081875df09ac24820f068ed9ed84b78fb
maddeth <20545155+maddeth@users.noreply.github.com>
2024-11-12 14:08:20 +0000

    update colours

--- src/colours.js
+++ src/colours.js
@@ -2,9 +2,7 @@ import { neon } from "@neondatabase/serverless"
 
 const sql = neon(import.meta.env.VITE_DATABASE_URL)
 
-export const coloursRowCount = async (req, res) => {
-    const result = await sql`SELECT version()`
-    const { version } = result[0]
-    res.writeHead(200, { "Content-Type": "text/plain" })
-    res.end(version)
+export const coloursRowCount = async () => {
+    const result = await sql`SELECT count(*) FROM colours;`
+    return result
 }
