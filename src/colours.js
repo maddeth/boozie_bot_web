f2522efe73d5ad9f5247d1485e3c9b9ce7bde53a
maddeth <20545155+maddeth@users.noreply.github.com>
2024-11-12 13:18:12 +0000

    test neon

--- src/colours.js
+++ src/colours.js
@@ -1,19 +1,10 @@
-import { TableClient, AzureSASCredential } from "@azure/data-tables"
+import { neon } from "@neondatabase/serverless"
 
-const account = import.meta.env.VITE_AZURE_TABLE_ACCOUNT
-const sas = import.meta.env.VITE_AZURE_STORAGE_SAS
+const sql = neon(import.meta.env.DATABASE_URL)
 
-const azureTableEndpoint = `https://${account}.table.core.windows.net`;
-const colourTableName = "colours";
-const colourTableClient = new TableClient(azureTableEndpoint, colourTableName, new AzureSASCredential(sas));
-
-export const coloursRowCount = parseInt(getRowCount(colourTableClient), 10)
-
-async function getRowCount(tableClient) {
-    let rowList = []
-    let entities = tableClient.listEntities();
-    for await (const entity of entities) {
-        rowList.push(entity.RowKey)
-    }
-    return rowList.length
+export const coloursRowCount = async (req, res) => {
+    const result = await sql`SELECT version()`
+    const { version } = result[0]
+    res.writeHead(200, { "Content-Type": "text/plain" })
+    res.end(version)
 }
