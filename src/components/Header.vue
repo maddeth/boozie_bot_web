b94f9b91c4582b2411e35b2b641647feb4690b58
maddeth <20545155+maddeth@users.noreply.github.com>
2024-11-14 14:43:27 +0000

    fix route

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -49,7 +49,7 @@ onMounted(async () => {
   <nav class="header-nav">
     <ul>
       <li><router-link to="/">Home</router-link></li>
-      <li><router-link to="/about">About</router-link></li>
+      <li><router-link to="/colours">Colours</router-link></li>
     </ul>
   </nav>
 </template>
