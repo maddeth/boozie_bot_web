f4319c27f9e962ca496c4e739331e84ff7f86b84
jon.smith <jon.smith@bjss.com>
2024-11-19 10:33:51 +0000

    update border on input

--- src/components/Colours.vue
+++ src/components/Colours.vue
@@ -80,6 +80,16 @@ async function submitColour() {
 </template>
 
 <style scoped>
+input {
+  &:valid {
+    border: 1px solid green;
+  }
+  
+  &:invalid {
+    border: 1px solid red;
+  }
+}
+
 h1 {
   font-weight: 500;
   font-size: 2.6rem;
