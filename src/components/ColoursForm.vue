a3327b819318c8c19ed9ffd0cd6614156a679653
maddeth <20545155+maddeth@users.noreply.github.com>
2024-11-21 10:37:12 +0000

    fix match

--- src/components/ColoursForm.vue
+++ src/components/ColoursForm.vue
@@ -30,16 +30,16 @@ onMounted(async () => {
 
 async function submitColour() {
   console.log(formData.value.colour)
-  const colour = formData.value.colour.toLowerCase().match(/[0-9a-z\s]{0,60}/g)
-  const hex = formData.value.hex.toUpperCase().match(/[0-9A-F]{6}/g)
+  const colour = formData.value.colour.toLowerCase().match(/[0-9a-z\s]{0,60}/g)[0]
+  const hex = formData.value.hex.toUpperCase().match(/[0-9A-F]{6}/g)[0]
   console.log(colour, hex)
   // colourAddResponse.value = await addColour(colour, hex, metadata.value.nickname)
   formData.value = getInitialData()
 }
 
 async function SearchByColour() {
-  const colour = searchColour.value.toLowerCase().match(/[0-9a-z\s]{0,60}/g)
-  console.log(searchColour)
+  const colour = searchColour.value.toLowerCase().match(/[0-9a-z\s]{0,60}/g)[0]
+  console.log(searchColour.value, colour)
   colourSearchResponse.value = await getByColourName(colour)
 }
 </script>
