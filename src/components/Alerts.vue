56247f121c6df80d03d922444275c650b58c244b
maddeth <20545155+maddeth@users.noreply.github.com>
2025-06-28 17:09:17 +0100

    test audio issue in alerts

--- src/components/Alerts.vue
+++ src/components/Alerts.vue
@@ -9,16 +9,18 @@ function start(server) {
   let socket = new WebSocket('wss://maddeth.com/websocket/');
   
   socket.addEventListener('open', function (event) {
+    console.log('WebSocket connected to:', server);
     socket.send('Web Client Connected');
   });
 
   socket.addEventListener('message', function (event) {
     var newEvent = JSON.parse(event.data)
-    console.log('Event from server ', newEvent.type);
+    console.log('Event from server:', newEvent.type, newEvent);
     //if (newEvent.type == "tts") {
       //  playTts(newEvent.id)
       //}
       if (newEvent.type == "redeem") {
+        console.log('Playing redeem audio:', newEvent.id);
         playRedeem(newEvent.id)
       }
     });
@@ -54,22 +56,39 @@ function playTts(id) {
 
 function dequeueAudio() {
   if (audioQueue.length > 0) {
-    const audio = new Audio(audioQueue.shift());
+    const audioUrl = audioQueue.shift();
+    console.log('Attempting to play audio:', audioUrl);
+    const audio = new Audio(audioUrl);
+    
     audio.onended = () => {
+      console.log('Audio ended');
       if (audioQueue.length > 0) {
         dequeueAudio();
       } else {
         isPlaying = false;
       }
     };
+    
     audio.onerror = (error) => {
+      console.error('Audio error:', error, 'URL:', audioUrl);
       if (audioQueue.length > 0) {
         dequeueAudio();
       } else {
         isPlaying = false;
       }
     };
-    audio.play();
+    
+    audio.play().then(() => {
+      console.log('Audio started playing');
+    }).catch((error) => {
+      console.error('Failed to play audio:', error);
+      // Try next audio or stop
+      if (audioQueue.length > 0) {
+        dequeueAudio();
+      } else {
+        isPlaying = false;
+      }
+    });
   }
 }
 </script>
